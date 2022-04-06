81f07bb7be2d0514c70227ab67d77696249fdc34
Sebastian Arambulo <earambulo.sa@gmail.com>
2022-04-05 23:08:39 -0400

    Add project files.

---
+++ Clase4/Controllers/EmpleadoController.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class EmpleadoController
+        Inherits Controller
+
+        ' GET: Empleado
+        Function Index() As ActionResult
+            Dim ListEmpleado As New List(Of Empleado)
+            Return View(ListEmpleado)
+        End Function
+
+        ' GET: Empleado/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Empleado/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Empleado/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Empleado/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Empleado/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Empleado/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Empleado/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
---
+++ Clase4/Models/Empleado.vb
@@ -0,0 +1,24 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class Empleado
+    <Required(ErrorMessage:="Debe ingresar su numero de documento")>
+    <MaxLength(6, ErrorMessage:="Debe tener hasta 6 caracteres el numero de documento")>
+    <Display(Name:="Nro. Documento")>
+    Public Property NroDocumento() As String
+    <Required(ErrorMessage:="Debe ingresar su nombre")>
+    Public Property Nombre() As String
+    <Required(ErrorMessage:="Debe ingresar su apellido")>
+    Public Property Apellido() As String
+    <Required(ErrorMessage:="Debe ingresar su dirección")>
+    <Display(Name:="Dirección")>
+    Public Property Direccion() As String
+    <Required(ErrorMessage:="Debe ingresar su fecha de nacimiento")>
+    <Display(Name:="Fecha de nacimiento")>
+    Public Property FechaNacimiento() As Date
+    <Required(ErrorMessage:="Debe ingresar su email")>
+    Public Property Email() As String
+    <Required(ErrorMessage:="Debe ingresar su salario")>
+    <Range(1000000, 15000000, ErrorMessage:="El salario debe ser entre 1.000.000 a 15.000.000")>
+    Public Property Salario() As Integer
+
+End Class
